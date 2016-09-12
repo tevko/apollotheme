67b74592fc7a2b70e93dfc0715b1b912b64ec7ff
Tim <timevko@gmail.com>
2016-09-11 22:42:34 -0400

    home page fields

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Apollo
+ */
+
+get_header(); ?>
+
+
+home page
+<?php
+get_footer();
