aac7439209652ab421bf042516c615592f1c64cf
Tim <timevko@gmail.com>
2016-09-13 12:18:25 -0400

    add homepage fields to template

--- page-home.php
+++ page-home.php
@@ -10,7 +10,42 @@
 
 get_header(); ?>
 
+<section class="hero">
+	<?php the_field('hero_image'); ?>
+	<?php the_field('hero_text_1'); ?>
+	<?php the_field('hero_text_2'); ?>
+</section>
 
-home page
+<section class="promo">
+	<?php if( have_rows('promo_boxes') ): ?>
+
+	    <?php while( have_rows('promo_boxes') ): the_row(); ?>
+
+	 		<article class="promoBox">
+	 			<?php the_sub_field('headline'); ?>
+	 			<?php the_sub_field('body_copy'); ?>
+	 			<?php the_sub_field('icon'); ?>
+	 			<?php the_sub_field('link'); ?>
+	 		</article>
+
+	    <?php endwhile; ?>
+
+	<?php endif; ?>
+
+	<article class="promoBox--Large">
+		<?php the_field('large_promo_box_headline'); ?>
+		<?php the_field('large_promo_box_body_copy'); ?>
+		<?php var_dump(the_field('large_promo_box_image')); ?>
+	</article>
+
+</section>
+
+<style>
+	section, article {
+		display: block;
+		margin: 20px;
+	}
+</style>
+
 <?php
 get_footer();
