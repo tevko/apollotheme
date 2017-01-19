c9c5634b09f14db1fdf55cdf447bae66293ff3f6
Tim <timevko@gmail.com>
2017-01-18 22:09:50 -0500

    fix sidebars

--- page-product.php
+++ page-product.php
@@ -14,7 +14,43 @@ get_header(); ?>
 		<p class="secondary heroText" style="background-image: url('<?php echo wp_get_attachment_image_src( get_field("page_hero_image"), "full" )[0] ?>')"><?php the_field('page_hero_text'); ?></p>
 	</section>
 	<!-- main page content -->
-	<main class="scaffold group">
+	<main class="scaffold group isProductPage">
+		<?php if( have_rows('sidebar') ): ?>
+			<aside class="Page-sidebar">
+				<ul>
+				    <?php while( have_rows('sidebar') ): the_row(); ?>
+				    	<?php
+				    		if (get_permalink() == get_sub_field('sidebar_link') || get_permalink($post->post_parent) == get_sub_field('sidebar_link')) {
+				    			$hidden = '';
+				    		} else {
+				    			$hidden = 'hidden';
+				    		}
+				    	?>
+
+				 		<li>
+				 			<a class="sidebar-link" href="<?php the_sub_field('sidebar_link'); ?>">
+				 				<?php the_sub_field('sidebar_text'); ?>
+				 			</a>
+				 			<?php while( have_rows('sub_links') ): the_row(); ?>
+				 				<li class="sidebar-sub-link <?php echo $hidden; ?>">
+				 					<a class="sidebar-link" href="<?php the_sub_field('sublink_url'); ?>">
+				 						<?php the_sub_field('sublink_text'); ?>
+				 					</a>
+				 				</li>
+				 			<?php endwhile; ?>
+				 		</li>
+
+				    <?php endwhile; ?>
+			    </ul>
+			</aside>
+		<?php endif; ?>
+		<div class="Page-content">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+	            <?php the_content(); ?>
+
+	        <?php endwhile; ?>
+        </div>
 		<?php if ( get_field('show_downloads_link') || get_field('show_request_product_link') ): ?>
 			<div class="product-options">
 				<?php if ( get_field('show_request_product_link') ): ?>
@@ -32,44 +68,7 @@ get_header(); ?>
 				<?php endif; ?>
 			</div>
 		<?php endif; ?>
-		<div class="Page-content">
-			<?php while ( have_posts() ) : the_post(); ?>
-
-	            <?php the_content(); ?>
-
-	        <?php endwhile; ?>
-        </div>
 		<!-- custom sidebar -->
-		<?php if( have_rows('sidebar') ): ?>
-		<aside class="Page-sidebar">
-			<ul>
-			    <?php while( have_rows('sidebar') ): the_row(); ?>
-			    	<?php
-			    		if (get_permalink() == get_sub_field('sidebar_link') || get_permalink($post->post_parent) == get_sub_field('sidebar_link')) {
-			    			$hidden = '';
-			    		} else {
-			    			$hidden = 'hidden';
-			    		}
-			    	?>
-
-			 		<li>
-			 			<a class="sidebar-link" href="<?php the_sub_field('sidebar_link'); ?>">
-			 				<?php the_sub_field('sidebar_text'); ?>
-			 			</a>
-			 			<?php while( have_rows('sub_links') ): the_row(); ?>
-			 				<li class="sidebar-sub-link <?php echo $hidden; ?>">
-			 					<a class="sidebar-link" href="<?php the_sub_field('sublink_url'); ?>">
-			 						<?php the_sub_field('sublink_text'); ?>
-			 					</a>
-			 				</li>
-			 			<?php endwhile; ?>
-			 		</li>
-
-			    <?php endwhile; ?>
-		    </ul>
-		</aside>
-
-		<?php endif; ?>
 	</main>
 
 <?php
